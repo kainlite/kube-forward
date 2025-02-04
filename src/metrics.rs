2583168f944d4b79e690fb2719d83d3ebbae6384
kainlite <kainlite@gmail.com>
2025-02-03 21:10:23 -0300

    fix: some updates and fixes

--- src/metrics.rs
+++ src/metrics.rs
@@ -11,18 +11,18 @@ impl ForwardMetrics {
     }
 
     pub fn record_connection_attempt(&self) {
-        counter!("port_forward_connection_attempts_total", 1, "forward" => self.name.clone());
+        counter!("port_forward_connection_attempts_total", 1, "name" => self.name.clone());
     }
 
     pub fn record_connection_success(&self) {
-        counter!("port_forward_connection_successes_total", 1, "forward" => self.name.clone());
+        counter!("port_forward_connection_successes_total", 1, "name" => self.name.clone());
     }
 
     pub fn record_connection_failure(&self) {
-        counter!("port_forward_connection_failures_total", 1, "forward" => self.name.clone());
+        counter!("port_forward_connection_successes_total", 1, "name" => self.name.clone());
     }
 
     pub fn set_connection_status(&self, connected: bool) {
-        gauge!("port_forward_connected", if connected { 1.0 } else { 0.0 }, "forward" => self.name.clone());
+        gauge!("port_forward_connected", if connected { 1.0 } else { 0.0 }, "name" => self.name.clone());
     }
 }
